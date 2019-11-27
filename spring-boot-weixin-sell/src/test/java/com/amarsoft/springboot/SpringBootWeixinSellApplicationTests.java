4775ba0b6a01b5c63ec3111834b23d9587cbfdd7
jiangshanwen <jiangshanwen@759036897@qq.com>
2019-11-28 01:28:56 +0800

    234234

---
+++ spring-boot-weixin-sell/src/test/java/com/amarsoft/springboot/SpringBootWeixinSellApplicationTests.java
@@ -0,0 +1,11 @@
+package com.amarsoft.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWeixinSellApplicationTests {
+	public static void main(String[] args) {
+      SpringApplication.run(SpringBootWeixinSellApplicationTests.class, args);
+	}
+}
