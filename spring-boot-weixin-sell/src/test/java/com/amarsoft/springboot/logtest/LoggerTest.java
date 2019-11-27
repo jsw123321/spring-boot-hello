64f90662f69e57c5bd46b6d3ffd2fcfde1499877
jiangshanwen <jiangshanwen@759036897@qq.com>
2019-11-28 00:45:13 +0800

    springboot-weixin-sell首次提交

---
+++ spring-boot-weixin-sell/src/test/java/com/amarsoft/springboot/logtest/LoggerTest.java
@@ -0,0 +1,42 @@
+/** 
+　 * Title: LoggerTest.java 
+　 * Description: TODO(一句话描述这个方法的目的和作用)
+　 * @author jiangshanwen 
+　 * @date 2019年11月27日上午1:30:55 
+　 * @version 1.0 
+*/
+
+package com.amarsoft.springboot.logtest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.apache.commons.logging.impl.SLF4JLog;
+/** 
+ * @Title: 
+ * @Description: （对类进行功能描述） 
+ * @author jiangshanwen
+ * @date: 2019年11月27日上午1:30:55 （日期） 
+ * @version:v.1.0
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@SpringBootConfiguration
+@Slf4j
+public class LoggerTest {
+    @Test
+	public  void loggerTest1() {
+	        String name = "linxz";
+	        String password = "123456";
+	        log.debug("debug...");
+	        log.info("name:{},password:{}", name, password);
+	        log.error("error...");
+
+	}
+
+}
